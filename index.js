b1eed46a3921742aeda1f4f29dbb0878b1707ef3
Hamza Ali <hamzali.tas.95@gmail.com>
2017-04-29 17:36:30 +0300

    Set up env

---
+++ index.js
@@ -0,0 +1,42 @@
+require('dotenv').config();
+
+const app = require('express')();
+const morgan = require('morgan');
+const bodyparser = require('body-parser');
+const mongoose = require('mongoose');
+
+// database connection.
+mongoose.connect('mongodb://' + process.env.DB_HOST);
+
+
+
+// Template and engine integration.
+app.engine('pug', require('pug').__express);
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+
+// middlewares
+app.use(bodyparser.json());
+
+app.use(morgan('dev'));
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+// App routes.
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('*', (req, res) => {
+    res.render('404');
+});
+
+
+
+
+app.listen(process.env.PORT, () => {
+    console.log('App listening on PORT:' + process.env.PORT);
+});
