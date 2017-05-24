dede417e6db01d0b28115b7a45ef2d57a186c84d
Hamza Ali <hamzali.tas.95@gmail.com>
2017-05-25 01:36:04 +0300

    db fix travis ci

--- src/configs/db.js
+++ src/configs/db.js
@@ -6,7 +6,8 @@ module.exports = app => {
     // MONGODB_URI=mongodb://root:root@ds123371.mlab.com:23371/heroku_g8dv2h6c
     mongoose.Promise = global.Promise;
     // console.log(process.env.MONGODB_URI = 'mongodb://db/prod');
-    mongoose.connect(process.env.MONGODB_URI || (process.env.MONGODB_DEV + process.env.NODE_ENV));
+    // mongoose.connect(process.env.MONGODB_URI || (process.env.MONGODB_DEV + process.env.NODE_ENV));
+    mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/' + 'test');
 
     mongoose.connection.on('error', (err) => {
         console.log('MongoDB Connection Error: ' + err);
