9d98169a02af39cbb14c0868ef630c82d2cd586d
Hamza Ali <hamzali.tas.95@gmail.com>
2017-05-25 11:59:45 +0300

    test db docker fix

--- src/configs/db.js
+++ src/configs/db.js
@@ -5,7 +5,8 @@ module.exports = app => {
     // database connection.
     // MONGODB_URI=mongodb://root:root@ds123371.mlab.com:23371/heroku_g8dv2h6c
     mongoose.Promise = global.Promise;
-    
+
+    if (process.env.NODE_ENV === 'test') process.env.MONGODB_URI = 'mongodb://localhost/test';
     // console.log(process.env.MONGODB_URI = 'mongodb://db/prod');
     mongoose.connect(process.env.MONGODB_URI || ('mongodb://db/' + process.env.NODE_ENV));
     // mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/' + 'test');
